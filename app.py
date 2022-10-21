e2603c605853dad497fc0a46b3c087d8a0759db9
Billy Chan <nf1bhc@mmgoverseas.federated.fds>
2022-10-21 17:02:13 +0800

    v01

--- app.py
+++ app.py
@@ -1,40 +1,40 @@
-#from flask import Flask
+import os
+from flask import Flask, render_template
 
-#app = Flask(__name__)
+app = Flask('MyHerokuApp')
 
-#@app.route("/")
+# Read the mailgun secret key from environment variables
+mailgun_secret_key_value = os.environ.get('MAILGUN_SECRET_KEY', None)
 
-
-#def index():
-#    print ("Hello World")
-
-
-#if __name__ == "__main__":
-#    app.run()
-
-
-from flask import Flask, redirect, url_for, request, render_template
-app = Flask(__name__ , static_url_path='')
+# This is needed for Heroku configuration, as in Heroku our
+# app will probably not run on port 5000, as Heroku will automatically
+# assign a port for our application
+port = int(os.environ.get("PORT", 5000))
 
 @app.route('/')
-def root():
-    return app.send_static_file('index.html')
- 
- 
-@app.route('/success/<name>')
-def success(name):
-    return 'welcome %s' % name
- 
- 
-@app.route('/login', methods=['POST', 'GET'])
-def login():
-    if request.method == 'POST':
-        user = request.form['nm']
-        return redirect(url_for('success', name=user))
-    else:
-        user = request.args.get('nm')
-        return redirect(url_for('success', name=user))
- 
- 
-if __name__ == '__main__':
-    app.run(debug=True)
+def index():
+
+    # We will just display our mailgun secret key, nothing more.
+    return render_template("index.html", value=mailgun_secret_key_value)
+
+# Route that will get the config value based on a provided key, so in
+# this way we can interogate our configuration.
+@app.route('/<config_key>')
+def key(config_key=None):
+    config_value=None
+
+    if (config_key):
+        # Advice: You should make a convention to define config key upper cased.
+        # otherwise you will have consistency issues when reading your keys.
+        # If you know they are always upper cased, you just need to uppercase
+        # what the config_key argument.
+        config_key = config_key.upper()
+        config_value = os.environ.get(config_key, None)
+
+    if key and config_value:
+        app.logger.info("Value of {} is {}.".format(config_key, config_value))
+
+    # We will just display our mailgun secret key, nothing more.
+    return render_template("keys.html", key=config_key, value=config_value)
+
+app.run(host='0.0.0.0', port=port, debug=True)
