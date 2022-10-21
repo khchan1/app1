b50a708122bf913da75e3be7169fabaca7c3203f
Billy Chan <nf1bhc@mmgoverseas.federated.fds>
2022-10-21 15:24:40 +0800

    v06

--- app.py
+++ app.py
@@ -6,7 +6,17 @@ app = Flask(__name__)
 
 
 def index():
-    return "Hello World!"
+    return ("""
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My First Heading</h1>
+<p>My first paragraph.</p>
+
+</body>
+</html>
+    """)
 
 if __name__ == "__main__":
     app.run()
