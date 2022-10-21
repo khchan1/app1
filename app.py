563b3c18c2324c2fbdfb6ddd5cb27725a3554126
Billy Chan <nf1bhc@mmgoverseas.federated.fds>
2022-10-21 15:53:40 +0800

    v08

--- app.py
+++ app.py
@@ -1,23 +1,40 @@
-from flask import Flask
+#from flask import Flask
 
+#app = Flask(__name__)
+
+#@app.route("/")
+
+
+#def index():
+#    print ("Hello World")
+
+
+#if __name__ == "__main__":
+#    app.run()
+
+
+from flask import Flask, redirect, url_for, request, render_template
 app = Flask(__name__)
 
-@app.route("/")
-
-
+@app.route('/')
 def index():
-    print ("Hello World")
-    return ("""
-<!DOCTYPE html>
-<html>
-<body>
-
-<h1>My First Heading</h1>
-<p>My first paragraph.</p>
-
-</body>
-</html>
-    """)
-
-if __name__ == "__main__":
-    app.run()
+    return ("console.log('Hello World')")
+ 
+ 
+@app.route('/success/<name>')
+def success(name):
+    return 'welcome %s' % name
+ 
+ 
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        user = request.form['nm']
+        return redirect(url_for('success', name=user))
+    else:
+        user = request.args.get('nm')
+        return redirect(url_for('success', name=user))
+ 
+ 
+if __name__ == '__main__':
+    app.run(debug=True)
